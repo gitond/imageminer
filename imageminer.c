e1527660a4215abdfde8f72599be68b799982d77
gitond <boti.ortutay@gmail.com>
2024-05-04 17:55:26 +0300

    Working qr-code reading function, uncleaned, undocumented

--- imageminer.c
+++ imageminer.c
@@ -1,4 +1,7 @@
 // C standard headers
+#include <stdio.h>	// For print debugging, remove later
+#include <string.h>
+#include <stdlib.h>
 
 // quirc library
 #include "quirc.h"			// For main library functionality
@@ -45,6 +48,37 @@ int imageQrCount(int argc, char **argv){
 
 char * imageContents(int argc, char **argv){
 	struct quirc *q; // Data location to hold qr-decoder
+	const int BUFFER_LENGTH = 2048;
+	char mainBuffer[BUFFER_LENGTH];
+	char tmpBuffer[BUFFER_LENGTH];
+	int processedLength = 0;
+	int totalLength = 0;
+
+/*
+	char* one = "quick";
+	char* two = "brown";
+	char* three = "fox";
+
+	processedLength = snprintf(mainBuffer, BUFFER_LENGTH,
+		"%s %s %s %s",
+		tmpBuffer, one, two, three
+	);
+
+	strncpy(tmpBuffer, mainBuffer, BUFFER_LENGTH);
+
+	processedLength = snprintf(mainBuffer, BUFFER_LENGTH,
+		"%s %s %s %s",
+		tmpBuffer, two, two, three
+	);
+
+	strncpy(tmpBuffer, mainBuffer, BUFFER_LENGTH);
+
+	if (processedLength >= BUFFER_LENGTH){
+		return "ERROR: amount of data in qr-codes exceeded 2048 byte buffer";
+	}
+
+	printf("Buffer: %s \n", mainBuffer);
+*/
 
 	if (argc < 2){
 		return "ERROR: incorrect usage. Specify file to test with.";
@@ -69,6 +103,55 @@ char * imageContents(int argc, char **argv){
 	}
 
 	quirc_end(q); // Necessary, because above code runs quirc_begin()
+
+	int count = quirc_count(q); // How many QR-codes in image?
+
+	for (int i = 0; i < count; i++){
+		struct quirc_code code;
+		struct quirc_data data;
+		quirc_decode_error_t err;
+
+		quirc_extract(q, i, &code); // should extract i:th code of image to memory address of code
+		err = quirc_decode(&code, &data); // should read the data from the qr-code at &code and save it to the memory adress of data. Any generated errors get saved to err.
+		if (err == QUIRC_ERROR_DATA_ECC) {
+			// Trying to flip and reread data on unsuccessful read
+			quirc_flip(&code); // QR-code mirrorind function as declared in quirc.h
+			err = quirc_decode(&code, &data);
+		}
+
+		// For debugging (On cleanup remove stdio)
+		if (data.payload_len > 0){
+
+			processedLength = snprintf(mainBuffer, BUFFER_LENGTH, "%s<payload id=\"%d\">%s</payload>",tmpBuffer,i+1,data.payload);
+			totalLength += processedLength;
+			if (totalLength >= BUFFER_LENGTH){
+				return "ERROR: amount of data in qr-codes exceeded 2048 byte buffer";
+			}
+			strncpy(tmpBuffer, mainBuffer, BUFFER_LENGTH);
+/*
+			printf("\n");
+			printf("Payload %d : %s \n", i+1, data.payload);
+			printf("Buffer: %s \n", mainBuffer);
+*/
+//			printf("No %d : %s \n", i+1, buffer);
+//			printf("First char of payload %d : %c \n", i+1, data.payload[0]);
+//			printf("Length of payload %d : %d \n", i+1, data.payload_len);
+
+			//return &data.payload[0];
+		}
+	}
+
+	if (totalLength > 0){
+		char *returnable = malloc((totalLength * sizeof(char)));
+		if (returnable) {
+			strncpy(returnable, mainBuffer, totalLength);
+
+			quirc_destroy(q);
+	//		return &mainBuffer[0];
+			return (returnable);
+		}
+		free(returnable);
+	}
 
 	quirc_destroy(q);
 	return "imageContents EOF";
